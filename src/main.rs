380355961eb98820820de67f07ea3a80b368e089
Yotam Medini <yotam.medini@gmail.com>
2024-11-29 23:05:52 +0200

    clap --begin under work

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,29 @@ fn parse_number(s: &str) -> Result<u32, String> {
         .map_err(|e| format!("Invalid number '{}': {}", s, e))
 }
 
+fn parse_milliseconds(_s: &str) -> Result<u32, String> {
+   Ok(13)
+}
+
 fn args_get_matches () -> clap::ArgMatches {
     let matches = command!() // requires `cargo` feature
         .version("0.1")
         .author("Yotam Medini Name <yotam.medini@gmail.com>") // unused ?
         .about("Modified Midi Player")
-        .arg(arg!([name] "Optional name to operate on")
-             .required(true)
-        )
         .arg(arg!(--seqdemo "Run hard-coded sequencer demo"))
+        .arg(arg!(-b --begin <TIME> "start time [minutes]:seconds[.millisecs]")
+            .required(false)
+            .value_parser(parse_milliseconds)
+        )
         .arg(
             arg!(--debugflags <FLAGS> "Debug bitwise flags")
             .required(false)
             .default_value("0")
             .value_parser(parse_number),
+        )
+        .arg(arg!([midifile] "The midi file to play")
+            .required(true)
+            .value_parser(value_parser!(PathBuf))
         )
         .get_matches();
     matches
@@ -37,6 +46,9 @@ fn main() {
         println!("seqdemo={}", seqdemo);
         if *seqdemo {
             sequencer::sequencer();
+            return;
         }
     }
+    let begin: u32 = *matches.get_one::<u32>("begin").unwrap_or(&0);
+    println!("begin={}", begin)
 }
