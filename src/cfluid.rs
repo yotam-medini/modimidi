85bfc8a526366c1b425437046973e79b6bef0a10
Yotam Medini <yotam.medini@gmail.com>
2024-12-06 16:01:10 +0200

    declare more cfluid extern C functions

--- src/cfluid.rs
+++ src/cfluid.rs
@@ -26,9 +26,14 @@ type FluidSeqId = i16; // fluid_seq_id_t
 extern "C" {
     pub fn delete_fluid_audio_driver(driver: *mut fluid_audio_driver_t);
     pub fn delete_fluid_event(evt: *mut fluid_event_t);
-    pub fn fluid_event_timer(evt: *mut fluid_event_t, data: *mut c_void);
     pub fn delete_fluid_sequencer(seq: *mut fluid_sequencer_t);
     pub fn delete_fluid_synth(synth: *mut fluid_synth_t);
+    pub fn fluid_event_note(
+        evt: *mut fluid_event_t,
+        channel: i32,
+        key: i16, 
+        vel: i16,
+        duration: u32);
     pub fn fluid_event_noteon(
         evt: *mut fluid_event_t,
         channel: i32,
@@ -36,16 +41,8 @@ extern "C" {
         vel: i16);
     pub fn fluid_event_set_dest(evt: *mut fluid_event_t, dest: FluidSeqId);
     pub fn fluid_event_set_source(evt: *mut fluid_event_t, src: FluidSeqId);
+    pub fn fluid_event_timer(evt: *mut fluid_event_t, data: *mut c_void);
     pub fn fluid_sequencer_get_tick(seq: *mut fluid_sequencer_t) -> u32;
-    pub fn fluid_settings_setint(
-        settings: *mut fluid_settings_t,
-	name: *const i8,
-	val: i32) -> i32;
-    pub fn new_fluid_event() -> *mut fluid_event_t;
-    pub fn new_fluid_settings() -> *mut fluid_settings_t;
-    pub fn new_fluid_audio_driver(
-        settings: *mut fluid_settings_t,
-        synth: *mut fluid_synth_t) -> *mut fluid_audio_driver_t;
     pub fn fluid_sequencer_register_client(
         seq: *mut fluid_sequencer_t, 
         name: *const i8,
@@ -63,10 +60,25 @@ extern "C" {
         evt: *mut fluid_event_t,
         time: u32,
         absolute: i32) -> i32;
+    pub fn fluid_settings_setint(
+        settings: *mut fluid_settings_t,
+	name: *const i8,
+	val: i32) -> i32;
+    pub fn fluid_synth_program_select(
+     	synth: *mut fluid_synth_t,
+        chan: i32,
+        sfont_id: i32,
+        bank_num: i32,
+        preset_num: i32) -> i32;
     pub fn fluid_synth_sfload(
         synth: *mut fluid_synth_t,
         path: *const i8,
         reset_presets: i32) -> i32;
+    pub fn new_fluid_event() -> *mut fluid_event_t;
+    pub fn new_fluid_settings() -> *mut fluid_settings_t;
+    pub fn new_fluid_audio_driver(
+        settings: *mut fluid_settings_t,
+        synth: *mut fluid_synth_t) -> *mut fluid_audio_driver_t;
     pub fn new_fluid_sequencer2(use_system_timer: i32)
         -> *mut fluid_sequencer_t;
     pub fn new_fluid_synth(settings: *mut fluid_settings_t)
