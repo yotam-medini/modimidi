07c41489ba48e10d012992b3135a6ee444f2b8ba
Yotam Medini <yotam.medini@gmail.com>
2026-04-04 12:04:50 +0300

    initial gmodimidi

---
+++ gmain.cpp
@@ -0,0 +1,43 @@
+#include <QApplication>
+#include <QMainWindow>
+#include <QToolBar>
+#include <QAction>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QMessageBox>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    QMainWindow window;
+    window.setWindowTitle("Minimal Qt Toolbar");
+    window.resize(400, 300);
+
+    // Create Toolbar
+    QToolBar *toolBar = window.addToolBar("Main Toolbar");
+
+    // Create Actions
+    QAction *fileAction = toolBar->addAction("File");
+    QAction *optionsAction = toolBar->addAction("Options");
+    toolBar->addSeparator();
+    QAction *exitAction = toolBar->addAction("Exit");
+
+    // Central Widget Label
+    QLabel *label = new QLabel("Welcome", &window);
+    label->setAlignment(Qt::AlignCenter);
+    window.setCentralWidget(label);
+
+    // Connect Actions (Lambda Functions)
+    QObject::connect(fileAction, &QAction::triggered, [&]() {
+        label->setText("File Action Clicked");
+    });
+
+    QObject::connect(optionsAction, &QAction::triggered, [&]() {
+        label->setText("Options Action Clicked");
+    });
+
+    QObject::connect(exitAction, &QAction::triggered, &app, &QApplication::quit);
+
+    window.show();
+    return app.exec();
+}
