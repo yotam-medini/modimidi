811a7e59e7b8399d6db73fb8b530d5eaf5d797c6
Yotam Medini <yotam.medini@gmail.com>
2026-04-15 10:33:51 +0300

    TimGM6mb.sf2 in Android APK loaded at runtime

--- gplay.cpp
+++ gplay.cpp
@@ -6,27 +6,33 @@
 #include <vector>
 #include "midi.h"
 #include "debug.h"
+#include "qutil.h"
 #include "synthseq.h"
+
 
 class GPlay::Impl {
  public:
-   Impl(bool is_android) : 
-     is_android_{is_android},
-     synseq_{"/usr/share/sounds/sf2/FluidR3_GM.sf2", 0x0} {
-     DebugMessage::AddMessage(std::format("synseq_.err={}", synseq_.error()));
-   }
-   std::string OpenMidi(std::vector<uint8_t> data) {
-     parsed_midi_ = std::make_unique<midi::Midi>(std::move(data));
-     auto err = parsed_midi_->GetError();
-     if (!err.empty()) {
-       parsed_midi_.reset();
-     }
-     return err;
-   }
-   std::unique_ptr<midi::Midi> parsed_midi_;
+  Impl(bool is_android) : 
+    is_android_{is_android},
+    synseq_{
+      is_android ? GetAndroidSoundFontPath(SF2_ANDROID) : SF2_DESKTOP, 
+      0x0} {
+    DebugMessage::AddMessage(std::format("synseq_.err={}", synseq_.error()));
+  }
+  std::string OpenMidi(std::vector<uint8_t> data) {
+    parsed_midi_ = std::make_unique<midi::Midi>(std::move(data));
+    auto err = parsed_midi_->GetError();
+    if (!err.empty()) {
+      parsed_midi_.reset();
+    }
+    return err;
+  }
+  std::unique_ptr<midi::Midi> parsed_midi_;
  private:
-   const bool is_android_;
-   SynthSequencer synseq_;
+  static constexpr auto SF2_DESKTOP = "/usr/share/sounds/sf2/FluidR3_GM.sf2";
+  static constexpr auto SF2_ANDROID = "TimGM6mb.sf2";
+  const bool is_android_;
+  SynthSequencer synseq_;
 };
 
 GPlay::GPlay(bool is_android) :
