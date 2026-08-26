aad04d6f51af4f744c4b1b0340969836943e3ef7
Yotam Medini <yotam.medini@gmail.com>
2026-08-26 21:52:38 +0300

    mixer += ComboBoxes

--- mixer.cpp
+++ mixer.cpp
@@ -1,15 +1,17 @@
 #include "mixer.h"
 #include <algorithm>
+#include <array>
+#include <QComboBox>
 #include <QFont>
 #include <QFrame>
 #include <QHBoxLayout>
 #include <QHeaderView>
-#include <QWidget>
 #include <QLabel>
 #include <QPushButton>
 #include <QSplitter>
 #include <QTableWidget>
 #include <QVBoxLayout>
+#include <QWidget>
 
 #include "gplay.h"
 #include "midi.h"
@@ -29,6 +31,7 @@ class Mixer::Impl {
   void CreateUI(QWidget *page);
   void SetTracksTable();
   void SetChannelsTable();
+  QWidget *CreateControlWidget(QWidget *parent, unsigned e_mixable, int i);
   GPlay &gplay_;
   QPushButton *default_buttons_[E_N]{nullptr, nullptr};
   QPushButton *silence_buttons_[E_N]{nullptr, nullptr};
@@ -110,7 +113,9 @@ void Mixer::Impl::SetTracksTable() {
         auto details_label = new QLabel(details, cell);
         layout->addWidget(name_label);
         layout->addWidget(details_label);
-        table->setCellWidget(row++, 0, cell);
+        table->setCellWidget(row, 0, cell);
+        table->setCellWidget(row, 1, CreateControlWidget(table, E_Tracks, i));
+        ++row;
       }
     }
     table->verticalHeader()->setVisible(false);
@@ -125,22 +130,47 @@ void Mixer::Impl::SetChannelsTable() {
     auto table = tables_[E_Channels];
     table->clear();
     channels_range_ = parsed_midi->GetChannels();
-    table->setHorizontalHeaderLabels({"Channel", "Volume Control"});
     table->setColumnCount(2);
+    table->setHorizontalHeaderLabels({"Channel", "Volume Control"});
     table->setRowCount(channels_range_.size());
     int row = 0;
-    for (const auto &item: channels_range_) {
-      const auto &range = item.second;
+    for (const auto &[ci, range]: channels_range_) {
       auto const details = std::format("{} Volume: {}",
         midi::MidiNoteRangeToString(range.notes_range_),
         midi::RangeToString(range.velocity_range_));
-      const auto s = qFormat("{:2d} {}", item.first, details);
-      table->setCellWidget(row++, 0, new QLabel(s, table));
+      const auto s = qFormat("{:2d} {}", ci, details);
+      table->setCellWidget(row, 0, new QLabel(s, table));
+      table->setCellWidget(row, 1, CreateControlWidget(table, E_Channels, ci));
+      ++row;
     }
     table->verticalHeader()->setVisible(false);
     table->resizeRowsToContents();
     table->resizeColumnsToContents();
   }
+}
+
+QWidget *Mixer::Impl::CreateControlWidget(
+  QWidget *parent,
+  unsigned e_mixable,
+  int i) {
+  qDebug() << qFormat("{} e_mixable={}, i={}\n", __func__, e_mixable, i);
+  auto w = new QWidget(parent);
+  auto layout = new QHBoxLayout(w);
+
+  QComboBox* combo = new QComboBox(w);
+  combo->addItem("Default");
+  combo->addItem("Silence");
+  combo->addItem("Custom");
+
+  connect(combo, QOverload<int>::of(&QComboBox::currentIndexChanged),
+    [this, e_mixable, i, combo](int index) {
+      qDebug() << "Selected" << combo->currentText() 
+               << "for" << e_mixable << i;
+  });
+
+  layout->addWidget(combo);
+  w->setLayout(layout);
+  return w;
 }
 
 Mixer::Mixer(QWidget *page, GPlay &gplay) :
